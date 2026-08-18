c39995a9dc72ba86142775ca9ec0b52bfd84c26b
Boluwatife Adeyemi <boluwatifeadeyemi64@gmail.com>
2026-08-18 15:34:36 +0100

    first bash script

---
+++ 0×00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Author:Boluwatife Adeyemi
+ * Program: WinMingle Community C Training
+ * Description:Introduction to C
+ * Hello world
+ */
+
+int main(){
+	printf("Hello world");
+
+	return 0;
+}
